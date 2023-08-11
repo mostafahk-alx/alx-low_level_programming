56bb530ab2181b47012412c07b4ce60c377b4ef1
Mostafa Hakkou <mostafahakkou1999@gmail.com>
2023-08-10 17:23:25 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 /**
- * main - This is main function
- * Return: should return  0 
- */
+* main - This is main function
+* Return: should return  0 
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
