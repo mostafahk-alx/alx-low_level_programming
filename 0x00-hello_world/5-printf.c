3114f9d059dc19990a1ec2a2d3d512225e5786da
Mostafa Hakkou <mostafahakkou1999@gmail.com>
2023-08-10 17:37:06 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - This is main function 
+ *
+ * Return: should return 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return 0;
+}
