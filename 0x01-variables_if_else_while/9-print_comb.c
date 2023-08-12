d62a737456086a0227edbd68f9e0439df0f5bd99
Mostafa Hakkou <mostafahakkou1999@gmail.com>
2023-08-11 18:15:10 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints all possible combinations of single-digit
+ *              numbers, separated by ", ", in ascending order.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num1, num2, num3;
+
+	for (num1 = 0; num1 <= 9; num1++)
+	{
+		for (num2 = num1; num2 <= 9; num2++)
+		{
+			for (num3 = num2; num3 <= 9; num3++)
+			{
+				if (num1 != num2 && num1 != num3 && num2 != num3)
+				{
+					putchar('0' + num1);
+					putchar('0' + num2);
+					putchar('0' + num3);
+
+					if (num1 != 7 || num2 != 8 || num3 != 9)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
