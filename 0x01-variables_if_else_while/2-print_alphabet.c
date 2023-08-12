59403aed8feb342212f7dbd1a52cb7f3ade0047c
Mostafa Hakkou <mostafahakkou1999@gmail.com>
2023-08-11 17:43:23 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints the lowercase alphabet
+ *              using the putchar function.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	// Start with the lowercase 'a'
+	char letter = 'a';
+
+	// Print the alphabet using a loop
+	while (letter <= 'z') {
+		putchar(letter);
+		letter++; // Move to the next letter
+	}
+
+	// Print a new line after the alphabet
+	putchar('\n');
+
+	return 0;
+}
