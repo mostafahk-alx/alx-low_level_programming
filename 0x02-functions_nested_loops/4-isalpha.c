a4128ee1a4f9f98d06c387f48c2f342de822b904
Mostafa Hakkou <mostafahakkou1999@gmail.com>
2023-08-15 18:19:41 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -11,10 +11,10 @@ int _isalpha(int c)
 	/* Check if c is a letter (lowercase or uppercase) */
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
-		return 1; /* c is an alphabetic character */
+		return (1); /* c is an alphabetic character */
 	}
 	else
 	{
-		return 0; /* c is not an alphabetic character */
+		return (0); /* c is not an alphabetic character */
 	}
 }
