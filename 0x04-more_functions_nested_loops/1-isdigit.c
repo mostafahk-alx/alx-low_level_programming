03303eb0bd515faa697210e9cdb19137d39a7fd4
Mostafa Hakkou <mostafahakkou1999@gmail.com>
2023-08-18 15:33:13 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+/*
+ * _isdigit - Check if char is a digit
+ *
+ * @c: Character
+ *
+ * Return: 1 if @c is a digit, 0 if @c is not a digit
+ */
+
+int _isdigit(int c){
+	if(n >= 48 && n <= 57)
+	{
+		return (1);
+	}
+	return (0);
+}
