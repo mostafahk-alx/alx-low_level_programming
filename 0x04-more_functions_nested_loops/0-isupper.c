24a9569ddd8707f56df718273da512d3fc316843
Mostafa Hakkou <mostafahakkou1999@gmail.com>
2023-08-18 15:39:55 -0700

    1-isdigit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,3 +1,10 @@
+/**
+ * _isupper - Checks if a char is uppercase 
+ * @c: The char to be checked
+ *
+ * Return: 1 if @c is upper, 0 if @c is lower, -1 if @c isn't a char
+ */
+
 int _isupper(int c)
 {
 	if(65>= c && c <= 90)
--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -7,7 +7,7 @@
  */
 
 int _isdigit(int c){
-	if(n >= 48 && n <= 57)
+	if(c >= 48 && c <= 57)
 	{
 		return (1);
 	}
