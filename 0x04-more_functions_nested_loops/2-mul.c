c713df7b5419c88061b7ac6b1b890969df9a42fe
Mostafa Hakkou <mostafahakkou1999@gmail.com>
2023-08-18 15:44:34 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+/*
+ * mul - Multiplies two numbers 
+ * @a: number1
+ * @b: number2
+ * @mul: result
+ * Return: @mul value
+ */
+
+int mul(int a, int b)
+{
+	int mul;
+	mul = a * b;
+	return (mul);
+}
