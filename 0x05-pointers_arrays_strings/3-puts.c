428bed873f10398981512bf96b7d53d5aa117cd0
Mostafa Hakkou <mostafahakkou1999@gmail.com>
2023-08-21 18:17:27 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+
+/**
+ * _puts - print a string followed by a new line
+ *
+ * @str: String var
+ *
+ */
+
+
+void _puts(char *str)
+{
+	int i;
+	
+	for(i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
