4c39f64fe244a6e567b700c78c910b1b3389e645
Mostafa Hakkou <mostafahakkou1999@gmail.com>
2023-08-21 18:39:10 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * rev_string - function that reverses given string
+ *
+ * @s: string var 
+ *
+ */
+
+void rev_string(char *s)
+{
+	int length = _strlen(s);
+	int i, j;
+	char temp;
+
+	for (i = 0, j = length - 1; i < j; i++, j--)
+	{
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
+	}
+}
