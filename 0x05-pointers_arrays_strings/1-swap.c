076f8ddca7663098d10a5d8cec8851713dcdd01f
Mostafa Hakkou <mostafahakkou1999@gmail.com>
2023-08-21 17:50:14 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+/**
+ * swap_int - function that swaps the values of two integers
+ *
+ * @a: variable one
+ * @b: variable two
+ *
+ */
+
+void swap_int(int *a, int *b)
+{
+	int *c;
+	*c = *a;
+	*a = *b;
+	*b = *c;
+}
