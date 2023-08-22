09206cf12bc423c455229320867835b0d6fc0c63
Mostafa Hakkou <mostafahakkou1999@gmail.com>
2023-08-21 19:04:07 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half - prints half of a string
+ *
+ * @str: string var
+ *
+ */
+
+void puts_half(char *str)
+{
+	int length = strlen(str);
+	int start_index;
+	int i = 0;
+
+	if(length % 2 == 0)
+	{
+		start_index = length / 2;
+	}
+	else
+	{
+		start_index = (length -1) / 2;
+	}
+
+	for(int i = start_index; i < length; i++)
+	{
+		_putchar(str[i]);
+	}
+}
