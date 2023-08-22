3f1b6c14ae708434bf286a0967334bd017b5dec9
Mostafa Hakkou <mostafahakkou1999@gmail.com>
2023-08-21 17:43:11 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+/**
+ * reset_to_98 - reset a var value to 98 using a pointer
+ *
+ * Return: 98
+ *
+ */
+
+void reset_to_98(int *n)
+{
+	*n=98;
+}
+
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+# alx-low_level_programminglearning about variables
+Pointers, arrays, and strings
