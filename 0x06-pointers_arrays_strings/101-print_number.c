40f86f069688113907b3af15d3c969b75fc69f23
Mostafa Hakkou <mostafahakkou1999@gmail.com>
2023-08-24 14:30:25 -0700

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,31 @@
+/**
+ * print_number - Prints an integer using _putchar.
+ * @n: The integer to be printed.
+ *
+ * Description: This function prints the given integer using only the _putchar
+ * function, without using long, arrays, pointers, or hard-coded values.
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	int divisor = 1;
+	int temp = n;
+
+	while (temp > 9)
+	{
+		divisor *= 10;
+		temp /= 10;
+	}
+
+	while (divisor > 0)
+	{
+		_putchar((n / divisor) + '0');
+		n %= divisor;
+		divisor /= 10;
+	}
+}
